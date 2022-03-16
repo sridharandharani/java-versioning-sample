6f40aa98a49e2dc58fce6657370c7021a68feffa
sridharandharani <sridharandharani1815@gmail.com>
2022-03-16 11:58:54 +0530

    addition

---
+++ src/com/harman/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.harman.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 3;
+        int y =2;
+        int c = x +y;
+        System.out.println(c);
+    }
+}
